f654d9d9dda796483c42dedbcc1d0366f4d44b13
Rian Pauzi <rianpauzi@Rians-MacBook-Pro.local>
2015-09-26 12:58:30 +0700

    first commit of calc

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,14 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var totalTextField : UITextField!
+    @IBOutlet var taxPctSlider : UISlider!
+    @IBOutlet var taxPctLabel : UILabel!
+    @IBOutlet var resultsTextView : UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        refreshUI()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func calculateTapped(sender : AnyObject) {
+        tipCalc.total = Double((totalTextField.text! as NSString).doubleValue)
+        
+        let possibleTips = tipCalc.returnPossibleTips()
+        var results = ""
+        
+        for (tipPct, tipValue) in possibleTips {
+            results += "\(tipPct)%: \(tipValue)\n"
+        }
+        
+        resultsTextView.text = results
+    }
+    
+    @IBAction func taxPercentageChanged(sender : AnyObject) {
+        tipCalc.taxPct = Double(taxPctSlider.value) / 100.0
+        refreshUI()
+    }
+    
+    @IBAction func viewTapped(sender : AnyObject) {
+        totalTextField.resignFirstResponder()
+    }
+    
+    let tipCalc = TipCalculatorModel(total: 33.25, taxPct: 0.06)
+    
+    func refreshUI() {
+        totalTextField.text = String(format: "%0.2f", tipCalc.total)
+        
+        taxPctSlider.value = Float(tipCalc.taxPct) * 100.0
+        
+        taxPctLabel.text = "Tax Percentage (\(Int(taxPctSlider.value))%)"
+        
+        resultsTextView.text = ""
     }
 
 
---
+++ TipCalculatorModel.swift
@@ -0,0 +1,39 @@
+//
+//  TipCalculatorModel.swift
+//  TipCalculator
+//
+//  Created by Rian Pauzi on 9/26/15.
+//  Copyright © 2015 Rian Pauzi. All rights reserved.
+//
+
+import Foundation
+
+class TipCalculatorModel {
+    var total: Double;
+    var taxPct: Double;
+    var subtotal: Double {
+        get {
+            return total / (taxPct + 1)
+        }
+    }
+    
+    init(total: Double, taxPct: Double) {
+        self.total = total;
+        self.taxPct = taxPct;
+    }
+    
+    func calcTipWithTipPct(tipPct: Double) -> Double {
+        return subtotal * tipPct;
+    }
+    
+    func returnPossibleTips() -> [Int:Double] {
+        let possibleTipsInferred = [0.15, 0.18, 0.20];
+        
+        var retval = [Int: Double]()
+        for possibleTip in possibleTipsInferred {
+            let intPct = Int(possibleTip*100)
+            retval[intPct] = calcTipWithTipPct(possibleTip)
+        }
+        return retval;
+    }
+}
